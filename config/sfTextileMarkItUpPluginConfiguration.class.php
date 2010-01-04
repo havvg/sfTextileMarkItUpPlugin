49e1bee27eefe45eb2acac6e652fe90c8d21e361
Toni Uebernickel <tuebernickel@gmail.com>
2010-01-04 19:36:59 +0100

    initial commit
    * added LICENSE and README files
    * added plugin configuration with dependency check
    * added markitup web assets
    * added sfTextilePreview module

---
+++ config/sfTextileMarkItUpPluginConfiguration.class.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * @author        Toni Uebernickel <toni@uebernickel.info>
+ * @link          http://toni.uebernickel.info/
+ *
+ * @package       sfTextileMarkItUpPlugin
+ * @subpackage    config
+ */
+
+/**
+ * The configuration of the sfTextileMarkItUpPlugin.
+ * Includes all dependencies.
+ */
+class sfTextileMarkItUpPluginPluginConfiguration extends sfPluginConfiguration
+{
+  static protected $DEPENDENCIES = array(
+    'sfTextilePlugin' => 'textile support',
+  );
+
+  /**
+   * @see sfPluginConfiguration
+   */
+  public function initialize()
+  {
+    $enabledPlugins = $this->configuration->getPlugins();
+
+    foreach (self::$DEPENDENCIES as $pluginName => $whatFor)
+    {
+      if (!in_array($pluginName, $enabledPlugins))
+      {
+        throw new sfConfigurationException(sprintf('You must install and enable plugin "%s" which provides %s.', $pluginName, $whatFor));
+      }
+    }
+
+    /* required for symfony 1.1 compatibility */
+    require dirname(__FILE__).'/config.php';
+  }
+}
---
+++ modules/sfTextilePreview/actions/actions.class.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Actions for previewing certain convertions.
+ *
+ * @package    sfTextileMarkItUpPlugin
+ * @subpackage preview
+ */
+class sfTextilePreviewActions extends sfActions
+{
+  /**
+   * Show the preview of a sent textile string.
+   * This is meant to be used on the basic markItUp! iframe.
+   *
+   * @param sfWebRequest $request
+   *
+   * @return string
+   */
+  public function executePreviewTextile(sfWebRequest $request)
+  {
+    $this->setLayout(false);
+    $this->executePreviewTextile()
+
+    $this->message = sfTextile::doConvert($request->getPostParameter('data'));
+
+    return sfView::SUCCESS;
+  }
+}
